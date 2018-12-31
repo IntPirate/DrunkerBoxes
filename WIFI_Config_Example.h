c0251769d155852e8e3d5de6d17d8cade4f43a14
IntPirate <IntPirate@github.com>
2018-12-30 19:12:02 -0600

    Created functions and files to make it easy to read.

---
+++ WIFI_Config_Example.h
@@ -0,0 +1,16 @@
+/*
+Please Rename to WIFI_Config.h 
+
+Here is an Example of the WIFI_Config.h file. 
+The reason I created this was so that I did not 
+have to keep chaging the WIFI Variables and 
+forgetting to change them to defualt befor 
+Pushing it to the Repo. 
+
+*/
+#include <Arduino.h>
+
+
+//WiFi Configuration, change these to your WiFi settings
+#define WiFi_AP "WIFI_NAME"
+#define WiFi_PW "WIFI_PASSWORD"
